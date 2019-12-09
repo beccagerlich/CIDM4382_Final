7656e27e3f6f35a0f096a28198c0e24aad857d37
Becca Gerlich <becca_gerlich@Beccas-MacBook-Pro-3.local>
2019-12-09 11:20:04 -0600

    added

--- VATSIMHarvester/harvest.js
+++ VATSIMHarvester/harvest.js
@@ -13,8 +13,7 @@ const task = cron.schedule('* /2 * * * *', () => {
    axios.get('https://api.darksky.net/forecast/72b7a195bb6fcb4a8f01cfaf880d0fef/34.9803,101.9188')
     .then( (response) => {
 
-        let _lat = response.data.latitude;
-        let _lon = response.data.longitude;
+        
         let _time = response.data.currently.time;
         let _temp = response.data.currently.temperature;
         let _humid = response.data.currently.humidity;
@@ -50,5 +49,37 @@ const task = cron.schedule('* /2 * * * *', () => {
         scheduled: false
     }
 );
+// const task2 = cron.schedule('* /2 * * * *', () => {
 
+//     axios.get('https://traffic.api.here.com/traffic/6.3/incidents/xml/8/134/86?app_id={ZF9ABGlCsYXVhvhMU5RH}&app_code={hdmllY-4vQcq9j_XYGhDxg}')
+//      .then( (response) => {
+ 
+         
+//          let _desc = response.data.abbreviation.description;
+//          
+ 
+//          var report = {
+//              desc: desc,
+//        
+//          }
+ 
+//          const uri = process.env.MONGODB_ATLAS_URL;
+ 
+//          mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+//                  .catch(err => console.log(err));
+    
+//         var promise = Weather.create(report, function (err, small) {
+//              if (err) return handleError(err);
+//              // saved!
+//          });
+ 
+//      })
+//      .catch( (error) => {
+//          console.log(error);
+//      });
+ 
+//      },{
+//          scheduled: false
+//      }
+//  );
 module.exports = task;
